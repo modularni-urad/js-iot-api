df6d987bbf2c440e04230f3b6487c0e75c1208d0
vencax <vencax@centrum.cz>
2019-11-19 19:49:13 +0100

    filtering

--- api/data.js
+++ api/data.js
@@ -1,7 +1,24 @@
 import _ from 'underscore'
+import { whereFilter } from 'knex-filter-loopback'
+
+function _getFilter (filter) {
+  try {
+    return JSON.parse(filter)
+  } catch (_) {
+    throw new Error('wrong filter')
+  }
+}
 
 export function find (cond, knex) {
-  return knex('envirodata').where(cond)
+  const filter = _getFilter(cond.filter)
+  if (!_.isObject(filter) || _.isEmpty(filter)) {
+    throw new Error('insufficient conditions')
+  }
+  let q = knex('envirodata')
+  q = cond.fields ? q.select(cond.fields.split(',')) : q
+  q = cond.limit ? q.limit(cond.limit) : q
+  // mozna tohle? https://github.com/Terminal-Systems/knex-flex-filter
+  return q.where(whereFilter(filter))
 }
 
 // NOT used now
