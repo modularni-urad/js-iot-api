3049292a328585eaba3d37f809b39d29ffa1d58e
vencax <vencax@centrum.cz>
2019-12-11 15:20:56 +0100

    refactor

--- api/devices.js
+++ api/devices.js
@@ -12,21 +12,24 @@ export function deviceManager (TTNApps) {
 }
 
 function _getAllDevices (apps) {
-  return Promise.all(_.map(apps, i => _getDevices(i[0], i[1]))).then(ress => {
-    return _.reduce(ress, (acc, i) => {
-      return _.union(acc, i)
-    }, [])
-  })
+  return Promise.all(_.map(apps, i => _getDevices(i[0], i[1])))
+    .then(ress => {
+      return _.reduce(ress, (acc, i) => {
+        return _.union(acc, i)
+      }, [])
+    })
 }
 
 // https://www.thethingsnetwork.org/docs/applications/manager/
 function _getDevices (app, key) {
   const url = `http://eu.thethings.network:8084/applications/${app}/devices`
   const opts = { headers: { 'Authorization': `Key ${key}` } }
-  return axios.get(url, opts).then(res => {
-    return _.map(res.data.devices, i => {
-      delete i.lorawan_device
-      return i
+  return axios.get(url, opts)
+    .then(res => {
+      return _.map(res.data.devices, i => (_.omit(i, 'lorawan_device')))
     })
-  })
+    .catch(err => {
+      console.error(`Fail to open: ${url}`)
+      throw err
+    })
 }
